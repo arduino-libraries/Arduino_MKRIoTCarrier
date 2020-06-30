1da85cc45b9c655ee50de0d95403d3bae2d4fa2e
marqdevx <11246294+marqdevx@users.noreply.github.com>
2020-06-30 14:54:37 +0300

    using Qtouch

--- src/Arduino_IOTSK_Qtouch.cpp
+++ src/Arduino_IOTSK_Qtouch.cpp
@@ -1,13 +1,128 @@
 
 #include <Arduino_IOTSK_Qtouch.h>
 
-IOTSKcarrier_Qtouch::IOTSKcarrier_Qtouch(int pin){
-    _pin = pin;
+
+//Individual pad
+IOTSKcarrier_Qtouch::IOTSKcarrier_Qtouch(int padIndex, IOTSKcarrier_Qtouch_Manager * pManager){
+    _padIndex = padIndex;
+    _pManager = pManager;
+
 }
 
+
 bool IOTSKcarrier_Qtouch::getTouch(){
-    if(analogRead(_pin) == 0){
-        return true;
+    bool getState = _pManager->t_state[_padIndex];
+    _saveToHistory(getState);
+    return(getState);
+}
+
+void IOTSKcarrier_Qtouch::_saveToHistory(bool newEntry){
+    //Serial.println(_padIndex);
+    for (int i = 10; i>0; i--){
+        _touches[i]  = _touches[i -1];
+       /* Serial.print(i);
+        Serial.print(" = ");
+        Serial.println( _touches[i]);*/
+        
     }
-    return false;
+    _touches[0] = newEntry;
+    /*
+    Serial.print(0);
+    Serial.print(" = ");
+    Serial.println( _touches[0]);*/
+    
+}
+
+bool IOTSKcarrier_Qtouch::onTouchDown(){
+    getTouch();
+    //Serial.print(_touches[0]);
+    //Serial.print(_touches[1]);
+    //Serial.println();
+    if(_touches[0] && !_touches[1]){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+bool IOTSKcarrier_Qtouch::onTouchUp(){
+    getTouch();
+    //Serial.print(_touches[0]);
+    //Serial.print(_touches[1]);
+    //Serial.println();
+    if(!_touches[0] && _touches[1]){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+
+bool IOTSKcarrier_Qtouch::onTouchChange(){
+    getTouch();
+    //Serial.print(_touches[0]);
+    //Serial.print(_touches[1]);
+    //Serial.println();
+    if(_touches[0] != _touches[1]){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+void IOTSKcarrier_Qtouch::updateConfig(int newSens, int newHyst){
+    setSensivity(newSens);
+    setHysteresis(newHyst);
+}
+
+//Manager
+bool IOTSKcarrier_Qtouch_Manager::begin(){
+    
+   
+    if (touch_sensors_init()){
+        return(false);
+    }
+
+    touch_time.current_time_ms = 0;
+    analogReadResolution(10);
+   
+}
+
+IOTSKcarrier_Qtouch_Manager::IOTSKcarrier_Qtouch_Manager(){
+
+}
+
+void IOTSKcarrier_Qtouch_Manager::update(){
+    
+    touch_time.time_to_measure_touch = 1;
+    touch_time.current_time_ms = touch_time.current_time_ms + millis();
+
+    touch_sensors_measure(); 
+
+    if ((p_selfcap_measure_data->measurement_done_touch == 1u)) {
+        p_selfcap_measure_data->measurement_done_touch == 0u;
+
+        t_state[0] = GET_SELFCAP_SENSOR_STATE(0);
+        t_state[1] = GET_SELFCAP_SENSOR_STATE(1);
+        t_state[2] = GET_SELFCAP_SENSOR_STATE(2);
+        t_state[3] = GET_SELFCAP_SENSOR_STATE(3);
+        t_state[4] = GET_SELFCAP_SENSOR_STATE(4);
+        
+        /*
+        Serial.print("Botton 1 status: ");
+        Serial.println(state1);
+        Serial.print("Botton 3 status: ");
+        Serial.println(state2);
+        Serial.print("Botton 3 status: ");
+        Serial.println(state3);
+        Serial.print("Botton 4 status: ");
+        Serial.println(state4);
+        Serial.print("Botton 5 status: ");
+        Serial.println(state5);
+        Serial.println();
+        */
+    }
+
+    rtcflag = false;
+
 }
--- src/Arduino_IOTSK_Qtouch.h
+++ src/Arduino_IOTSK_Qtouch.h
@@ -2,16 +2,57 @@
 #define ARDUINO_IOTSKcarrier_Qtouch
 
 #include "Arduino.h"
+#include "Arduino_Qtouch.h"
+
+
+//manager
+class IOTSKcarrier_Qtouch_Manager{
+    public:
+    IOTSKcarrier_Qtouch_Manager(); 
+
+    bool rtcflag = false;
+    
+        
+    bool begin();
+    void update();
+
+    int t_state[5];
+
+    private:
+
+
+};
 
 class IOTSKcarrier_Qtouch{
     public:
-    IOTSKcarrier_Qtouch(int pin);
-    int getPin();
+    IOTSKcarrier_Qtouch(int padIndex, IOTSKcarrier_Qtouch_Manager * pManager); //Individual
+    
+    int getPad();
 
     bool getTouch();
+    bool onTouchDown();
+    bool onTouchUp();
+    bool onTouchChange();
     
+    //Set touch settings
+    
+    void updateConfig(int newRes, int newHyst);
+
     private:
-    int _pin;
+    
+    IOTSKcarrier_Qtouch_Manager  * _pManager;
+    int _padIndex;  
+
+    bool _touches[10];                //To know last touches
+
+    void _saveToHistory(bool newEntry);
+
+    //Cfg
+    bool setOnChange = false;       //Touch on change
+    bool setOnNormal = false;       //Allways read
+    bool setOnUp = false;           //When the pad is not being touched
+    bool setOnDown = false;         //When is pressed, to make 2 Downs you need to touch it, release it and touch it again
+
 };
 
 #endif
