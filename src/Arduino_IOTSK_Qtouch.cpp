7cbc09a77c89664d0a58b3df5c99ad5b92ccd404
marqdevx <11246294+marqdevx@users.noreply.github.com>
2020-07-01 18:22:09 +0300

    Changes for the "cleaned" Arduino_Qtouch

--- src/Arduino_IOTSK_Qtouch.cpp
+++ src/Arduino_IOTSK_Qtouch.cpp
@@ -1,12 +1,12 @@
 
 #include <Arduino_IOTSK_Qtouch.h>
+#include "Arduino_Qtouch.h"
 
 
 //Individual pad
 IOTSKcarrier_Qtouch::IOTSKcarrier_Qtouch(int padIndex, IOTSKcarrier_Qtouch_Manager * pManager){
     _padIndex = padIndex;
     _pManager = pManager;
-
 }
 
 
@@ -23,14 +23,12 @@ void IOTSKcarrier_Qtouch::_saveToHistory(bool newEntry){
        /* Serial.print(i);
         Serial.print(" = ");
         Serial.println( _touches[i]);*/
-        
     }
     _touches[0] = newEntry;
     /*
     Serial.print(0);
     Serial.print(" = ");
     Serial.println( _touches[0]);*/
-    
 }
 
 bool IOTSKcarrier_Qtouch::onTouchDown(){
@@ -44,7 +42,6 @@ bool IOTSKcarrier_Qtouch::onTouchDown(){
         return false;
     }
 }
-
 bool IOTSKcarrier_Qtouch::onTouchUp(){
     getTouch();
     //Serial.print(_touches[0]);
@@ -56,8 +53,6 @@ bool IOTSKcarrier_Qtouch::onTouchUp(){
         return false;
     }
 }
-
-
 bool IOTSKcarrier_Qtouch::onTouchChange(){
     getTouch();
     //Serial.print(_touches[0]);
@@ -71,58 +66,24 @@ bool IOTSKcarrier_Qtouch::onTouchChange(){
 }
 
 void IOTSKcarrier_Qtouch::updateConfig(int newSens){
-    setSensivity(newSens);
-    //setHysteresis(newHyst);
+    TOUCH.setSensorsSensitivity(newSens);
+    //TOUCH.setSernsorsHysteresis(newHyst);
 }
 
 //Manager
 bool IOTSKcarrier_Qtouch_Manager::begin(){
-    
-   
-    if (touch_sensors_init()){
-        return(false);
-    }
-
-    touch_time.current_time_ms = 0;
-    analogReadResolution(10);
-   
+return TOUCH.begin();
 }
-
 IOTSKcarrier_Qtouch_Manager::IOTSKcarrier_Qtouch_Manager(){
 
 }
-
 void IOTSKcarrier_Qtouch_Manager::update(){
-    
-    touch_time.time_to_measure_touch = 1;
-    touch_time.current_time_ms = touch_time.current_time_ms + millis();
-
-    touch_sensors_measure(); 
-
-    if ((p_selfcap_measure_data->measurement_done_touch == 1u)) {
-        p_selfcap_measure_data->measurement_done_touch == 0u;
-
-        t_state[0] = GET_SELFCAP_SENSOR_STATE(0);
-        t_state[1] = GET_SELFCAP_SENSOR_STATE(1);
-        t_state[2] = GET_SELFCAP_SENSOR_STATE(2);
-        t_state[3] = GET_SELFCAP_SENSOR_STATE(3);
-        t_state[4] = GET_SELFCAP_SENSOR_STATE(4);
-        
-        /*
-        Serial.print("Botton 1 status: ");
-        Serial.println(state1);
-        Serial.print("Botton 3 status: ");
-        Serial.println(state2);
-        Serial.print("Botton 3 status: ");
-        Serial.println(state3);
-        Serial.print("Botton 4 status: ");
-        Serial.println(state4);
-        Serial.print("Botton 5 status: ");
-        Serial.println(state5);
-        Serial.println();
-        */
+    TOUCH .pool();
+    if(TOUCH.available()){
+        t_state[0] = TOUCH.read(0);
+        t_state[1] = TOUCH.read(1);
+        t_state[2] = TOUCH.read(2);
+        t_state[3] = TOUCH.read(3);
+        t_state[4] = TOUCH.read(4);
     }
-
-    rtcflag = false;
-
 }
