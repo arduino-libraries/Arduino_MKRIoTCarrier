d64bac80127c2c90c88454ee8a3a183189c28cae
marqdevx <11246294+marqdevx@users.noreply.github.com>
2020-08-12 13:52:37 +0200

    Update Arduino_MKRIoTCarrier.cpp

--- src/Arduino_MKRIoTCarrier.cpp
+++ src/Arduino_MKRIoTCarrier.cpp
@@ -35,10 +35,9 @@ int MKRIoTCarrier::begin(){
     //Sensors
     uint8_t sensorsOK = 0;
 
-	 if(!Light.begin()){
+    if(!Light.begin()){
         sensorsOK |= 0b1 << 0;
     }
-	Light.enableColor();
     if(!Preassure.begin()){
         sensorsOK |= 0b1 << 1;
     }
