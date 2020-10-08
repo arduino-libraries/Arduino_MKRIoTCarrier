5ba9bad9d03cb1158f4c6f623fa9678f4cfc1407
marqdevx <11246294+marqdevx@users.noreply.github.com>
2020-10-08 10:47:40 +0200

    Added Grove Intputs example

---
+++ examples/Grove_Inputs/Grove_Inputs.ino
@@ -0,0 +1,36 @@
+#include <Arduino_MKRIoTCarrier.h>
+MKRIoTCarrier carrier; //Constructor of the carrier maybe we can include it on the library itself
+
+//Set the pad sense distance for using them with or without the enclosure
+bool CARRIER_CASE = false;
+
+//Be sure that the Pins are the same as your wiring
+int moisture = A5;
+int moist;
+int pir = A6;
+bool trigger = false;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  while(!Serial);
+  carrier.begin();
+  pinMode(moisture,INPUT);
+  pinMode(pir,INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //Save readings
+  moist = analogRead(moisture);
+  trigger = digitalRead(pir);
+  Serial.println("Reading grove inputs A5 and A6");
+  Serial.print("Moist: ");
+  Serial.print(moist);
+  Serial.print("\t Move state: ");
+  Serial.println(digitalRead(pir));
+  Serial.println();
+  //delay(00);
+
+}
