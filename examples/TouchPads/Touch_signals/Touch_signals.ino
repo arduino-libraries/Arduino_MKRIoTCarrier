2344db1e8d2b0a2e21a6cf03ebbd13c5afdfdc4c
marqdevx <11246294+marqdevx@users.noreply.github.com>
2020-12-16 16:38:53 +0100

    Added signals check example

---
+++ examples/TouchPads/Touch_signals/Touch_signals.ino
@@ -0,0 +1,28 @@
+// Touch Buttons test
+
+#include <Arduino_MKRIoTCarrier.h>
+MKRIoTCarrier carrier;
+
+int msr[5];
+
+void setup() {
+  // put your setup code here, to run once:
+  //CARRIER_CASE = false;     //No needed to use it, default false
+  Serial.begin(9600);
+  carrier.begin();
+  
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for (int i = 0; i < 5; i++) {
+    msr[i] = analogRead(i);
+    Serial.print(i);
+    Serial.print(" - ");
+    Serial.print(msr[i]);
+    Serial.print("\t");
+  }
+  Serial.println();
+  
+}
