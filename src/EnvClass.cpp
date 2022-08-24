76d5be34e80008f3e546a5f1e3ee3d3db772e7fe
Riccardo Rizzo <37290960+Rocketct@users.noreply.github.com>
2022-08-24 17:46:29 +0200

    Update EnvClass.cpp

--- src/EnvClass.cpp
+++ src/EnvClass.cpp
@@ -24,7 +24,7 @@ extern Bme68x* BME;
 // sets function called on slave write
 EnvClass::EnvClass( getRev_t getRevision )
 {
-  //If board_revision = 1, IMU module is LSM6DSOX, otherwhise is LSM6DS3
+  //If board_revision = 1, IMU module is LSM6DSOX, otherwise is LSM6DS3
   board_revision = getRevision;
 }
 
